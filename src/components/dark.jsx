9eb20ed61f7dbe89c20c1f302871620979dd10bc
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2022-10-21 00:58:13 +0200

    inicio de proyecto

---
+++ src/components/dark.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+const Dark=()=>{
+    return(
+        <div>
+            <h1>Este es el modo oscuro</h1>
+        </div>
+    )
+}
+export default Dark;
---
+++ src/components/light.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+const Light=()=>{
+    return(
+        <div className="light_container">
+            <div class="ligth_container_container">
+                <div class="light_container_title">
+                    <h1>TODO</h1>
+                </div>
+                <div class="light_container_button">
+                    <button><image src="./assets/images/icon-moon.svg" alt="imagen_boton_claro"></image></button>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Light;
